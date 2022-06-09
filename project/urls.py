e304d261d9a0f4e4d6a88ed4f57653846051044d
mattccs <mattcc.shek@gmail.com>
2022-06-09 16:23:14 +0100

    jwt_auth complete, other apps base files created

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/',include('jwt_auth.urls')),
+    path('api/albums/',include('albums.urls')),
+    path('api/tracks/',include('tracks.urls')),
+    path('api/genres/',include('genres.urls')),
+    path('api/artists/',include('artists.urls')),
+    path('api/tracklist/',include('tracklist.urls')),
+    path('api/userplaylist/',include('user_playlist.urls')),
 ]
