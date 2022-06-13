bbbb87fccd79a0e049627215585f219e276e0e73
mattccs <mattcc.shek@gmail.com>
2022-06-13 16:46:45 +0100

    seeded some data

--- tracklists/models.py
+++ tracklists/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Tracklist(models.Model):
-  tracks = models.ForeignKey('tracks.Track',  on_delete=models.CASCADE)
+  tracks = models.ForeignKey('tracks.Track', blank=True, on_delete=models.CASCADE)
 
   def __str__(self):
     return f"{self.tracks}"
--- userPlaylist/models.py
+++ userPlaylist/models.py
@@ -6,3 +6,6 @@ class UserPlaylist(models.Model):
   name = models.CharField(max_length=30, default='new playlist')
   owner = models.ForeignKey('jwt_auth.User', related_name='owner', on_delete=models.CASCADE)
   tracks = models.ManyToManyField('tracks.track', blank=True)
+
+  def __str__(self):
+    return f"{self.name}"
