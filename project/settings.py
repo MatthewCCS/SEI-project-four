895829084455370f206aa646bf30634cb0757144
mattccs <mattcc.shek@gmail.com>
2022-07-11 02:09:13 -0700

    ready to deploy

--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+
+import django_on_heroku
 """
 Django settings for project project.
 
@@ -9,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_on_heroku
+
 import os
 from pathlib import Path
 
@@ -134,6 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
