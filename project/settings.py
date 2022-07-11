69c3717429ae791d311ad28d6c52866635385218
mattccs <mattcc.shek@gmail.com>
2022-07-11 10:27:48 -0700

    ready for deploy

--- project/settings.py
+++ project/settings.py
@@ -53,7 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'rest_framework',
     'jwt_auth',
     'albums',
