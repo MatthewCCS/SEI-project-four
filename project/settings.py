f7c8649ee35f322d90dfd34186fe3675b45e3d01
mattccs <mattcc.shek@gmail.com>
2022-07-11 09:18:42 -0700

    deploying...

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-environ.Env.read_env(env_file=str(BASE_DIR) + '/.env') 
 SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
